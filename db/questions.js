f5482194f7f85d536ee73f6bb970b253cb17f7d1
akbarovmir <mirabrorakbarov@gmail.com>
2021-06-26 16:03:10 +0500

    home page

--- db/questions.js
+++ db/questions.js
@@ -1,7 +1,7 @@
 const db = [
   {
     "id": 1,
-    "question": "This is question about JavaScript. This is long text. How should fit container"
+    "question": "This is question about JavaScript. This is long text. How should fit container. This is question about JavaScript. This is long text. How should fit container."
   },
   {
     "id": 2,
--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,31 @@ const cardContainer = document.getElementById('cardContainer');
 
 const Card = (data) => {
   const card = document.createElement('div');
-  card.classList.add('card')
-  card.textContent = data.question;
+  card.classList.add('card');
+
+  const p = document.createElement('p');
+  p.textContent = data.question;
+
+  const button = document.createElement('button');
+  button.textContent = 'Answer';
+  button.onclick = () => {
+    window.location.href = `/question.html?id=${data.id}`;
+  };
+
+  card.appendChild(p);
+  card.appendChild(button);
+
   return card;
 };
 
 window.onload = () => {
-  db.forEach((cardData) => {
+  if (!localStorage.getItem('questions')) {
+    localStorage.setItem('questions', JSON.stringify(db))
+  }
+  
+  let question = JSON.parse(localStorage.getItem('questions'))
+  
+  question.forEach((cardData) => {
     cardContainer.appendChild(Card(cardData));
   });
 };
