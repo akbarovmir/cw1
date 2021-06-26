cb8c87a60695f2922dcca3d1299c6772c8689b24
akbarovmir <mirabrorakbarov@gmail.com>
2021-06-27 00:35:24 +0500

    db

--- db/questions.js
+++ db/questions.js
@@ -1,26 +1,52 @@
 const db = [
   {
-    "id": 1,
-    "name": "Akbarov",
-    "question": "This is question about JavaScript. This is long text. How should fit container. This is question about JavaScript. This is long text. How should fit container.",
-    "answers": []
+    id: 1,
+    name: 'Akbarov',
+    question:
+      'What is JavaScript and its use? Is JavaScript user for backend or frontend? Best users of JavaSript?',
+    answers: [
+      {
+        owner: 'John',
+        text: 'JavaScript mostly used on Web Development',
+      },
+      {
+        owner: 'Jane',
+        text: 'You can also build mobile and desktop applications with JavaScript👍',
+      },
+    ],
   },
   {
-    "id": 2,
-    "name": "Akbarov",
-    "question": "This is question about JavaScript",
-    "answers": []
+    id: 2,
+    name: 'Akbarov',
+    question: 'What is the difference between ES6 and ES7?',
+    answers: [
+      {
+        owner: 'John',
+        text: 'There are additional syntax change on ES7',
+      },
+    ],
   },
   {
-    "id": 3,
-    "name": "John",
-    "question": "This is question about JavaScript",
-    "answers": []
+    id: 3,
+    name: 'John',
+    question: 'Where to save cookies? Is localStorage safe?',
+    answers: [
+      {
+        owner: 'Jane',
+        text: 'I think localStorge is not safe for keeping sensitive data.',
+      },
+    ],
   },
   {
-    "id": 4,
-    "name": "Jane",
-    "question": "This is question about JavaScript",
-    "answers": []
-  }
-]
+    id: 4,
+    name: 'Jane',
+    question: 'What are the changes on Windows 11?',
+    answers: [],
+  },
+  {
+    id: 5,
+    name: 'Mark',
+    question: 'Which is better React or Angular? Give me some tips.',
+    answers: [],
+  },
+];
